53389584965fbe4857f624a56a4cfcb2a5a8f4d6
ye-seob <jakalob103@naver.com>
2025-02-04 21:49:05 +0900

    [FEAT] prisma 연동

--- src/service/dummy.service.ts
+++ src/service/dummy.service.ts
@@ -1,18 +1,7 @@
-import {
-  AlreadyExistError,
-  InvalidInputError,
-  NotFoundError,
-} from "../util/error.js";
+import { addUser } from "../repository/dummy.repositroy.js";
 
 export const dummyService = async (data: string) => {
-  if (data === "성공 응답") return "성공";
-  else if (data === "실패 응답1") {
-    throw new AlreadyExistError("이미 존재하는 유저입니다", "1");
-  } else if (data === "실패 응답2") {
-    throw new NotFoundError("존재 하지않는 유저입니다", "1");
-  } else if (data === "실패 응답3") {
-    throw new InvalidInputError("잘못된 입력 값입니다", "1");
-  }
+  const dummy = await addUser();
 
-  return "성공";
+  return dummy;
 };
