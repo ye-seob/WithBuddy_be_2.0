83edc4ffc097794f90d670184642f3faceea11ea
ye-seob <jakalob103@naver.com>
2025-02-27 16:09:50 +0900

    [FEAT] #8 회원가입 라우트 연동

--- src/service/dummy.service.ts
+++ src/service/dummy.service.ts
@@ -1,7 +1,5 @@
-import { addUser } from "../repository/dummy.repositroy.js";
-
 export const dummyService = async (data: string) => {
-  const dummy = await addUser();
+  const dummy = 1;
 
   return dummy;
 };
