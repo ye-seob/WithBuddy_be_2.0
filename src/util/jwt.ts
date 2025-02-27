c7b84cae5d080948409d97a25fdf90c833d6093d
ye-seob <jakalob103@naver.com>
2025-02-27 22:39:46 +0900

    [FEAT] #10 JWT 미들웨어 설정

---
+++ src/util/jwt.ts
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const SECRET_ACCESS_KEY = process.env.JWT_SECRET_ACCESS_KEY!;
+const SECRET_REFRESH_KEY = process.env.JWT_SECRET_REFRESH_KEY!;
+
+export const generateAccessToken = (payload: any) => {
+  return jwt.sign(payload, SECRET_ACCESS_KEY, { expiresIn: "3h" });
+};
+
+export const generateRefreshToken = (payload: any) => {
+  return jwt.sign(payload, SECRET_REFRESH_KEY, { expiresIn: "2d" });
+};
+
+export const verifyToken = (token: string) => {
+  return jwt.verify(token, SECRET_ACCESS_KEY);
+};
