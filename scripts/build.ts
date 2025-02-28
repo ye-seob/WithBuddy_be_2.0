a0ba1797c2d98cbcfa2aa28530f5953fac985f3b
ye-seob <jakalob103@naver.com>
2025-03-01 00:08:42 +0900

    Swagger 설정

--- scripts/build.ts
+++ scripts/build.ts
@@ -10,4 +10,5 @@ await esbuild.build({
   target: "node20",
   minify: true,
   keepNames: true,
+  external: ["swagger-ui-express", "yamljs"],
 });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import cors from "cors";
 import express from "express";
 import { errorMiddleware, successMiddleware } from "./util/middleware.js";
 import mainRouter from "./routes/route.index.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 const app = express();
 
@@ -16,9 +19,12 @@ app.use(
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
+const swaggerSpec = YAML.load(path.join("./build/swagger.yaml"));
 app.use(successMiddleware);
 
 app.use("/api/v1", mainRouter);
+
+app.use("/swagger-ui", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorMiddleware);
 
