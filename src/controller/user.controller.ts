3e4236703a890ab0e24d7d60dfd0e3e59a012fb9
ye-seob <jakalob103@naver.com>
2025-03-11 23:37:44 +0900

    쿠키  설정

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -87,7 +87,6 @@ export const loginController = async (
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: false, // HTTP에서는 secure: false로 설정
-      sameSite: "lax", // HTTP에서는 "lax"로 설정
       domain, // 환경에 맞는 도메인 설정
       maxAge: 3 * 60 * 60 * 1000,
       path: "/",
@@ -96,7 +95,6 @@ export const loginController = async (
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: false, // HTTP에서는 secure: false로 설정
-      sameSite: "lax", // HTTP에서는 "lax"로 설정
       domain, // 환경에 맞는 도메인 설정
       maxAge: 2 * 24 * 60 * 60 * 1000,
       path: "/",
